e0170d99ac7128a8b84b96a1d83ba887aea624b3
over-rover <gh.pueblo@gmail.com>
2023-12-17 21:16:41 +0300

    HW5rel03: Реализован класс MapStorage и тесты к нему.

---
+++ src/webapp/storage/MapStorage.java
@@ -0,0 +1,48 @@
+package webapp.storage;
+
+import webapp.model.Resume;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapStorage extends AbstractStorage {
+    protected final Map<String, Resume> storage = new HashMap<>();
+
+    public void clear() {
+        storage.clear();
+        System.out.println("\nВыполнена очистка HashMap");
+    }
+
+    public Resume[] getAll() {
+        return storage.values().toArray(new Resume[0]);
+    }
+
+    public int size() {
+        return storage.size();
+    }
+
+    protected Object getSearchKey(String uuid) {
+        return uuid;
+    }
+
+    protected boolean isExist(Object searchKey) {
+        return storage.containsKey((String) searchKey);
+    }
+
+    protected void doDelete(Object searchKey) {
+        storage.remove((String) searchKey);
+    }
+
+    protected void doSave(Resume r, Object searchKey) {
+        storage.put((String) searchKey, r);
+    }
+
+    protected final Resume doGet(Object searchKey) {
+        return storage.get((String) searchKey);
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Object searchKey) {
+        storage.replace((String) searchKey, r);
+    }
+}
---
+++ test/webapp/storage/MapStorageTest.java
@@ -0,0 +1,7 @@
+package webapp.storage;
+
+public class MapStorageTest extends AbstractStorageTest {
+    public MapStorageTest() {
+        super(new MapStorage());
+    }
+}
